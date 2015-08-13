e3c858d024184be80beedc78cf4601a732c1433a
pijoules <lchan1994@yahoo.com>
2015-08-13 14:22:34 -0400

    centered fraction numerators and denominators

--- Fraction.py
+++ Fraction.py
@@ -23,6 +23,8 @@ class Fraction(Command):
 		zero_index = 1
 		arg0_has_args = self.line_has_args(args[0])
 		arg1_has_args = self.line_has_args(args[1])
+		max_num_len = 0
+		max_den_len = 0
 
 		if arg0_has_args:
 			sub_map, sub_map_zero = Lexer.Lexer().parse_line(args[0])
@@ -31,10 +33,12 @@ class Fraction(Command):
 			del disp[0]
 			for line in sub_map[::-1]:
 				disp.insert(0,line)
+				max_num_len = max(max_num_len,len(line))
 
 			zero_index += len(sub_map)-1 # Adjust the zero index
 		else:
 			disp[0] = disp[0].replace("$0", args[0])
+			max_num_len = len(disp[0])
 
 		if arg1_has_args:
 			sub_map, sub_map_zero = Lexer.Lexer().parse_line(args[1])
@@ -43,9 +47,21 @@ class Fraction(Command):
 			del disp[-1]
 			for line in sub_map:
 				disp.append(line)
+				max_den_len = max(max_den_len,len(line))
 		else:
 			disp[-1] = disp[-1].replace("$1", args[1])
+			max_den_len = len(disp[-1])
 
-		disp[zero_index] = disp[zero_index]*max([len(x) for x in disp])
+		# Stretch out the line
+		max_line_len = max(max_num_len,max_den_len)
+		num_diff = max_line_len-max_num_len
+		den_diff = max_line_len-max_den_len
+		disp[zero_index] = disp[zero_index]*max_line_len
+
+		# Center the numerator and denominator
+		for i in range(zero_index):
+			disp[i] = " "*(num_diff/2) + disp[i]
+		for i in range(zero_index+1,len(disp)):
+			disp[i] = " "*(den_diff/2) + disp[i]
 
 		return [disp, zero_index]
